f65eb4cedf2d80008143b0e1c3cd01c48a4cc081
cvicens <cvicens@mbp-cvicens.local>
2015-11-14 18:00:43 +0100

    Version

--- rest-logger.js
+++ rest-logger.js
@@ -5,14 +5,18 @@ function RestLogger (config) {
   self.config = config;
 
   this.sendTempData = function (sensorId, temp, presence, callback) {
-    console.log('config: ' + JSON.stringify(self.config));
+    //console.log('[FINER] config: ' + JSON.stringify(self.config));
     // Get user profile
     var authorization = "Basic " + new Buffer(self.config.user + ":" + self.config.password).toString("base64");
 
     var url = self.config.TEMPERATURE_SERVICE_URL;
+    console.log('url: ' + url);
     url = url.replace(/\:sensorId/g, sensorId);
+    console.log('url after 1: ' + url);
     url = url.replace(/\:temp/g, temp);
+    console.log('url after 2: ' + url);
     url = url.replace(/\:presence/g, presence);
+    console.log('url after 1: ' + url);
 
     var options = {
       url: url,
